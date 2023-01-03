e31c0bc9ff3fc246cd2730410013c075773b7e79
X-MOD-I <deepanshumodi2000@gmail.com>
2023-01-03 22:08:51 +0530

    all testcases passed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,48 @@
-const User = require("../schema/user.schema");
+const User = require('../schema/user.schema');
 
 module.exports.getUsersWithPostCount = async (req, res) => {
+  let { page = 1, limit = 10 } = req.query;
+  page = +page;
+  limit = +limit;
   try {
-    //TODO: Implement this API
+    const totalDocs = await User.count();
+    const totalPages = Math.ceil(totalDocs / limit);
 
-    res.status(200).json({});
+    const users = await User.aggregate([
+      { $skip: limit * (page - 1) },
+      { $limit: limit },
+      { $project: { _id: 1, name: 1 } },
+      {
+        $lookup: {
+          from: 'posts',
+          localField: '_id',
+          foreignField: 'userId',
+          as: 'posts',
+        },
+      },
+      {
+        $addFields: {
+          posts: { $size: '$posts' },
+        },
+      },
+    ]);
+
+    res.status(200).json({
+      data: {
+        users: users,
+        pagination: {
+          totalDocs,
+          limit,
+          page,
+          totalPages,
+          pagingCounter: limit * (page - 1) + 1,
+          hasPrevPage: page === 1 ? false : true,
+          hasNextPage: page < totalPages ? true : false,
+          prevPage: page === 1 ? null : page - 1,
+          nextPage: page < totalPages ? page + 1 : null,
+        },
+      },
+    });
   } catch (error) {
     res.send({ error: error.message });
   }
